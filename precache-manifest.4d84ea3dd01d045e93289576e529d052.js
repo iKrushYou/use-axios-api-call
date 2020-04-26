ba1d9d0bf7ecd6f90ed75696556aa79186acf25d
alexkrush <alex-krush-github>
2020-04-26 18:38:51 -0400

    Updates

---
+++ precache-manifest.4d84ea3dd01d045e93289576e529d052.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2e968b4ad861883b9169659299d4c1f",
+    "url": "/use-axios-api-call/index.html"
+  },
+  {
+    "revision": "4b50fc8b60443864dbd2",
+    "url": "/use-axios-api-call/static/css/main.6faa9ad1.chunk.css"
+  },
+  {
+    "revision": "7f804bbb0f03d55ff034",
+    "url": "/use-axios-api-call/static/js/2.85950daf.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/use-axios-api-call/static/js/2.85950daf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b50fc8b60443864dbd2",
+    "url": "/use-axios-api-call/static/js/main.574d3f78.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "/use-axios-api-call/static/js/main.574d3f78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a0573d86a28d4f92963",
+    "url": "/use-axios-api-call/static/js/runtime-main.1acc4910.js"
+  }
+]);
